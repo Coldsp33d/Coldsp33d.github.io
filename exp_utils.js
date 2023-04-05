7bbc0e91e09b80f5d4c8286dd3c0500f7000a40a
Shiva Deviah <shivakdeviah@gmail.com>
2023-04-05 13:53:04 +0530

    correctly parse numbers before calculation

--- exp_utils.js
+++ exp_utils.js
@@ -31,46 +31,46 @@ function expInBillAdd(exp, level) {
 }
 
 function calculateLevel2Exp() {
-  const level = document.getElementById("levelInput").value;
+  const level = Number(document.getElementById("levelInput").value);
   const exp = level2Exp(level);
   document.getElementById("levelExpOutput").value = exp;
 }
 
 function calculateLevel2ExpInBillion() {
-  const level = document.getElementById("levelInputBillion").value;
+  const level = Number(document.getElementById("levelInputBillion").value);
   const exp = level2ExpInBillion(level);
   document.getElementById("levelExpOutputBillion").value = exp;
 }
 
 function calculateExp2Level() {
-  const exp = document.getElementById("expInput").value;
+  const exp = Number(document.getElementById("expInput").value);
   const level = exp2Level(exp);
   document.getElementById("expLevelOutput").value = level;
 }
 
 function calculateExpInBillion2Level() {
-  const exp = document.getElementById("expInputBillion").value;
+  const exp = Number(document.getElementById("expInputBillion").value);
   const level = expInBillion2Level(exp);
   document.getElementById("expLevelOutputBillion").value = level;
 }
 
 function calculateLevelDifference() {
-  const level1 = document.getElementById("levelInput1").value;
-  const level2 = document.getElementById("levelInput2").value;
+  const level1 = Number(document.getElementById("levelInput1").value);
+  const level2 = Number(document.getElementById("levelInput2").value);
   const levelDiff = levelDifference(level1, level2);
   document.getElementById("levelDiffOutput").value = levelDiff;
 }
 
 function calculateExpAdd() {
-  const exp = document.getElementById("expInput2").value;
-  const level = document.getElementById("levelInput3").value;
+  const exp = Number(document.getElementById("expInput2").value);
+  const level = Number(document.getElementById("levelInput3").value);
   const newLevel = expAdd(exp, level);
   document.getElementById("expLevelOutput2").value = newLevel;
 }
 
 function calculateExpInBillAdd() {
-  const exp = document.getElementById("expInput3").value;
-  const level = document.getElementById("levelInput4").value;
+  const exp = Number(document.getElementById("expInput3").value);
+  const level = Number(document.getElementById("levelInput4").value);
   const newLevel = expInBillAdd(exp, level);
   document.getElementById("expLevelOutput3").value = newLevel;
 }
